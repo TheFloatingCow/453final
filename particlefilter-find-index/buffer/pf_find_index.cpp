71243e69a06c65a61efe6b3e67e5f7d1a0e106b3
Kenji MacMartin <kenji_macmartin@sfu.ca>
2026-03-31 14:14:58 -0700

    try stuff ig in buffer

--- particlefilter-find-index/buffer/pf_find_index.cpp
+++ particlefilter-find-index/buffer/pf_find_index.cpp
@@ -2,36 +2,49 @@
 
 static void load_vector(const data_t* src, data_t dst[MAX_PARTICLES], int n_particles) {
     #pragma HLS INLINE off
-    load_vector_loop: for (int i = 0; i < n_particles; ++i) {
+
+    for (int i = 0; i < MAX_PARTICLES; ++i) {
         #pragma HLS PIPELINE II=1
-        dst[i] = src[i];
+        if (i < n_particles) {
+            dst[i] = src[i];
+        }
     }
 }
 
 static void load_tile(const data_t* src, data_t dst[PARTICLE_TILE], int base, int tile_count) {
     #pragma HLS INLINE off
-    load_tile_loop: for (int i = 0; i < tile_count; ++i) {
+
+    for (int i = 0; i < PARTICLE_TILE; ++i) {
         #pragma HLS PIPELINE II=1
-        dst[i] = src[base + i];
+        if (i < tile_count) {
+            dst[i] = src[base + i];
+        }
     }
 }
 
 static void store_tile(data_t* dst, const data_t src[PARTICLE_TILE], int base, int tile_count) {
     #pragma HLS INLINE off
-    store_tile_loop: for (int i = 0; i < tile_count; ++i) {
+
+    for (int i = 0; i < PARTICLE_TILE; ++i) {
         #pragma HLS PIPELINE II=1
-        dst[base + i] = src[i];
+        if (i < tile_count) {
+            dst[base + i] = src[i];
+        }
     }
 }
 
 static int find_first_geq(const data_t cdf[MAX_PARTICLES], int n_particles, data_t value) {
     #pragma HLS INLINE off
-    int index = n_particles - 1;
 
-    find_index_loop: for (int i = 0; i < n_particles; ++i) {
-        if (cdf[i] >= value) {
+    int index = n_particles - 1;
+    bool found = false;
+
+
+    for (int i = 0; i < MAX_PARTICLES; ++i) {
+        #pragma HLS PIPELINE II=1
+        if (i < n_particles && !found && cdf[i] >= value) {
             index = i;
-            break;
+            found = true;
         }
     }
 
@@ -50,10 +63,13 @@ static void compute_tile(
 ) {
     #pragma HLS INLINE off
 
-    compute_tile_loop: for (int j = 0; j < tile_count; ++j) {
-        int index = find_first_geq(cdf_buf, n_particles, u_buf[j]);
-        xj_buf[j] = array_x_buf[index];
-        yj_buf[j] = array_y_buf[index];
+
+    for (int j = 0; j < PARTICLE_TILE; ++j) {
+        if (j < tile_count) {
+            int index = find_first_geq(cdf_buf, n_particles, u_buf[j]);
+            xj_buf[j] = array_x_buf[index];
+            yj_buf[j] = array_y_buf[index];
+        }
     }
 }
 
@@ -72,6 +88,7 @@ extern "C" void find_index_kernel(
     #pragma HLS INTERFACE m_axi port=array_y offset=slave bundle=gmem3 depth=16384
     #pragma HLS INTERFACE m_axi port=xj offset=slave bundle=gmem4 depth=16384
     #pragma HLS INTERFACE m_axi port=yj offset=slave bundle=gmem5 depth=16384
+
     #pragma HLS INTERFACE s_axilite port=cdf bundle=control
     #pragma HLS INTERFACE s_axilite port=u bundle=control
     #pragma HLS INTERFACE s_axilite port=array_x bundle=control
@@ -89,6 +106,7 @@ extern "C" void find_index_kernel(
     data_t yj_buf[PARTICLE_TILE];
 
     int particle_count = n_particles;
+
     if (particle_count <= 0) {
         return;
     }
@@ -100,15 +118,22 @@ extern "C" void find_index_kernel(
     load_vector(array_x, array_x_buf, particle_count);
     load_vector(array_y, array_y_buf, particle_count);
 
-    tile_loop: for (int base = 0; base < particle_count; base += PARTICLE_TILE) {
-        int tile_count = PARTICLE_TILE;
-        if (base + tile_count > particle_count) {
-            tile_count = particle_count - base;
-        }
+    const int NUM_TILES = MAX_PARTICLES / PARTICLE_TILE;
 
-        load_tile(u, u_buf, base, tile_count);
-        compute_tile(cdf_buf, array_x_buf, array_y_buf, u_buf, xj_buf, yj_buf, particle_count, tile_count);
-        store_tile(xj, xj_buf, base, tile_count);
-        store_tile(yj, yj_buf, base, tile_count);
+
+    for (int t = 0; t < NUM_TILES; ++t) {
+        int base = t * PARTICLE_TILE;
+
+        if (base < particle_count) {
+            int tile_count = PARTICLE_TILE;
+            if (base + tile_count > particle_count) {
+                tile_count = particle_count - base;
+            }
+
+            load_tile(u, u_buf, base, tile_count);
+            compute_tile(cdf_buf, array_x_buf, array_y_buf, u_buf, xj_buf, yj_buf, particle_count, tile_count);
+            store_tile(xj, xj_buf, base, tile_count);
+            store_tile(yj, yj_buf, base, tile_count);
+        }
     }
 }
